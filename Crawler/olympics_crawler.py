72e3ab826023230eaf7c9ae598db678c7520ba55
Shivam Gupta <sadam9099@gmail.com>
2020-04-13 01:42:59 -0500

    Crawler with Downloading webpages added

---
+++ Crawler/olympics_crawler.py
@@ -0,0 +1,72 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+import time
+from url_download import download_webpage
+import os
+from datetime import datetime
+
+start_time =datetime.now()
+
+class OlympicSpider(scrapy.Spider):
+    name = "olympic"
+    start_urls = [
+        'https://www.olympic.org',
+    ]
+    
+    
+    count_links = 0
+    olympics_links = [] 
+    queue = []
+
+    exploration = True
+    MAX_LINK_COUNT = 10000
+    visited = 0
+
+    def extract_and_enqueue_links(self, response):
+
+        print("Vising URL: ",response.url)
+        with open('linkslists.txt', 'a') as filehandle:
+            filehandle.write('%s\n' % response.url)
+        # Downloading this webpage from the URL
+        download_webpage(response.url)
+        self.visited += 1
+        print("Visited count: ", self.visited)
+        self.olympics_links.append(response.url)
+
+        title = response.css('title::text').get()
+        content = response.css('p::text').getall()
+    
+        if self.exploration is True:  
+
+            links = LinkExtractor(canonicalize=True, unique=True).extract_links(response)  
+
+            self.queue.extend(links)    
+
+            self.count_links += len(links)
+
+            if self.count_links >= self.MAX_LINK_COUNT:
+                self.exploration = False
+
+    
+
+    def parse(self, response):
+        
+        self.extract_and_enqueue_links(response)
+
+        while len(self.queue) != 0:
+
+            print("Queue size: ", len(self.queue))
+            # print(self.visited)
+            time.sleep(0.001)   
+
+            link = self.queue.pop(0)
+
+            yield response.follow(link, self.extract_and_enqueue_links)
+
+
+end_time =datetime.now()  
+        
+print("Total Time for crawling and downloading the webpages: ",end_time-start_time)
+
+   # How to Run the file: scrapy runspider olympics_crawler.py
+#    TODO Implement with BFS
---
+++ Crawler/url_download.py
@@ -0,0 +1,40 @@
+# import urllib.request, urllib.error, urllib.parse
+import nltk   
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import os
+webpage_dir = r"Webpages"
+        
+def download_webpage(url):
+
+    
+    html = urlopen(url).read()    
+    # html = html.decode('utf-8')
+
+    # html = urllib.urlopen(url).read()
+    soup = BeautifulSoup(html)
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
+
+    # get text
+    text = soup.get_text()
+
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    txt = '\n'.join(chunk for chunk in chunks if chunk)
+    text = txt.encode('unicode-escape').decode('utf-8')
+    text = text.replace("\n", " ")
+    # print(text)
+    page = url.split("/")[-1] + ".txt"
+    filename = os.path.join(webpage_dir, page)
+    with open(filename, 'w') as filehandle:
+                filehandle.write('%s\n' % text)
+
+
+if __name__ == '__main__':
+    url = 'https://www.olympic.org/los-angeles-1932' 
+    download_webpage(url)
