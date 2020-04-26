e3d5a92e0112dc5286c8ac108ed5f61aa70e07ba
Wasif Haque <wasifhaque@Wasifs-MacBook-Pro.local>
2020-04-26 00:19:19 -0500

    fixed pipeline based on reading inputs from json

--- cluster.py
+++ cluster.py
@@ -1,130 +1,57 @@
-import pickle, os
+import pickle, os, sys
 import numpy as np
 from collections import Counter
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, AgglomerativeClustering
+from nltk.stem import WordNetLemmatizer
 from sklearn.metrics.pairwise import cosine_similarity 
+import pickler 
+import textprocessor as tp
+
+# RUN python cluster.py 0 / 1 kmeans / agg
 
 def main():
 
-	load = True
+	load = bool(int(sys.argv[1]))
+	clustering_type = sys.argv[2]
+	n_clusters = int(sys.argv[3])
+	# print(load)
 
+	
+	doc_vectors = pickler.unpickle_item('docvec')
 	print('Document vectors are loaded!')
-	doc_vectors = unpickle_item('docvec')
 	print(doc_vectors.shape)
 
-	vectorizer = unpickle_item('vectorizer')
+	vectorizer = pickler.unpickle_item('vectorizer')
 	print('vectorizer is loaded')
 
-	doc2vec = map_doc_to_vector(doc_vectors) # for getting the corresponding tfidf vector for each documents
-
-	# print(doc2vec)
-	search_query = ['olympic winner london']
-
-	## TODO : apply text processing on search query
-
-	transformed_query = vectorizer.transform(search_query)
+	doc_contents = np.array(pickler.unpickle_item('doc_contents'))
+	url_list = np.array(pickler.unpickle_item('url_list'))
 	
 	
 	if load is True:
-		kmeans = unpickle_item('cluster')
+		kmeans = pickler.unpickle_item('cluster')
 	else:
-		kmeans = KMeans(n_clusters=10, random_state=0).fit(doc_vectors)
-		pickle_item(kmeans, 'cluster')
-
-		## TODO : apply agglomerative clustering
+		if clustering_type == 'kmeans':
+			cluster = KMeans(n_clusters=n_clusters, random_state=0).fit(doc_vectors)
+			pickler.pickle_item(cluster, 'cluster')
+		elif clustering_type == 'agg':
+			cluster = AgglomerativeClustering(n_clusters=n_clusters, random_state=0).fit(doc_vectors)
+			pickler.pickle_item(cluster, 'cluster')
 
 	
-	labels = np.array(kmeans.labels_)
+	labels = np.array(cluster.labels_)
 	
-	doc_list = np.array(os.listdir('Webpages'))
+	# doc_list = np.array(os.listdir('Webpages'))
 
 	webpage_cluster = {}
 
 	# assign clusters to documents 
 	for label in np.unique(labels):
-		webpage_cluster[label] = doc_list[ np.where(labels == label) ]
+		doc_indices = np.where(labels == label)
+		webpage_cluster[label] = (url_list[ doc_indices] , doc_contents[ doc_indices ] )
 
-	## CLUSTERING DONE
-
-
-	## QUERY EXPANSION ##
-
-	# all the documents from the predicted cluster
-	predicted_docs = webpage_cluster[ kmeans.predict(transformed_query)[0]]
-
-	# get the additional terms 
-	new_terms = expand_query(transformed_query, predicted_docs, doc2vec, vectorizer)
-
-	print(new_terms)
-
-
-def expand_query(query, docs, doc2vec, vectorizer):
-
-	scores = np.array([cosine_similarity(query, doc2vec[doc])[0][0] for doc in docs])
-	
-	# get the top 10 document indices by cosine score
-	top_indices = np.argsort(scores)[-10:]
-
-	doc_list = os.listdir('Webpages/')
-
-	# dictionary for mapping terms to their weights
-	max_term_weights = {}
-	
-	for i in top_indices:
-
-		doc_name = doc_list[i]	
-
-		vector = doc2vec[ doc_name ] # tfidf vector for a document
-
-		terms = vectorizer.inverse_transform(vector)[0]	# get the terms corresponding to those tfidf weights
-		nonzero_vector = np.array(vector[ vector.nonzero() ])[0]	# convert the sparse matrix to an array with only the weights
-
-
-		# assign the max weights of a term to that term in the dictionary
-		for t,ti in zip(terms, range(len(terms))):
-
-			if t in max_term_weights:
-				if nonzero_vector[ti] > max_term_weights[t]:
-					max_term_weights[t] = nonzero_vector[ti]
-			else:
-				max_term_weights[t] = nonzero_vector[ti]
-
-	top_terms = Counter(max_term_weights).most_common(5) 	# get the top 5 terms with highets ewight
-
-	return [term[0] for term in top_terms]
-
-
-
-
-
-def map_doc_to_vector(doc_vectors):
-
-	doc2vec = {}
-
-	doc_list = os.listdir('Webpages')
-
-	for i,dname in zip(range(len(doc_list)), doc_list):
-
-		doc2vec[ dname ] = doc_vectors[i]
-
-	return doc2vec 
-
-
-
-def unpickle_item(filename):
-	
-	infile = open(filename, 'rb')
-	unpickled_item = pickle.load(infile)
-	infile.close()
-
-	return unpickled_item
-
-
-def pickle_item(item, filename):
-
-	outfile = open(filename, 'wb')
-	pickle.dump(item, outfile)
-	outfile.close()
+	pickler.pickle_item(webpage_cluster, 'webpage_cluster')
+	print('Clustering complete and serialized')
 
 
 if __name__ == "__main__":
--- vectorize.py
+++ vectorize.py
@@ -4,18 +4,42 @@ from collections import Counter
 from sklearn.feature_extraction.text import TfidfVectorizer 
 from nltk.stem.porter import *
 from nltk.corpus import stopwords
-
-
+import textprocessor as tp
+import pickler
+import json
 
 def main():
 
-    path = 'Crawler/Webpages/'
-    tokens = tokenize_docs(path)
-   
-    filtered_tokens = remove_stopwords(tokens)
+    path = 'index.json'
 
-    # stems = stem_tokens(filtered_tokens)
+    with open(path, encoding="utf8") as json_file:
+        res = json.load(json_file)
+
+    docs = res['response']['docs']
+
+    url_list = []
+    doc_contents = []
+
+    for doc in docs:
+        if 'url' and 'content' in doc:
+            url_list.append(doc['url'])
+            doc_contents.append(doc['content'])
+
+    pickler.pickle_item(url_list, 'url_list')
+    pickler.pickle_item(doc_contents, 'doc_contents')
+
+    print('Text Processing . . .') 
+    tokens = tp.tokenize_docs(doc_contents)
+   
+    filtered_tokens = tp.remove_stopwords(tokens)
+    print('Done.')
+
+    print(len(filtered_tokens))
+    # stems = tp.tem_tokens(filtered_tokens)
+
+    print('Apply vectorizer . . ')
     apply_vectorization(filtered_tokens)
+    print('Done.')
 
 
 def apply_vectorization(tokens):
@@ -29,107 +53,9 @@ def apply_vectorization(tokens):
     
     doc_vectors = vectorizer.transform(processed_docs)
 
-    print(doc_vectors)
 
-    pickle_item(doc_vectors, 'docvec')
-    pickle_item(vectorizer, 'vectorizer')
-    
-
-
-def remove_stopwords(tokens):
-
-	stop_words = set(stopwords.words('english'))
-
-	filtered_tokens = []
-
-	for token in tokens:
-		filtered_tokens.append([t for t in token if t not in stop_words and len(t) > 1])
-
-	return filtered_tokens
-
-
-def stem_tokens(tokens):
-
-    stemmer = PorterStemmer()
-    
-    stems = []
-
-    for token in tokens:
-    	stems.append([stemmer.stem(t) for t in token])
-
-
-    return stems
-
-
-def tokenize_docs(path):
-	"""Iterates through the list of documents, processes and tokenizes text
-	
-	Args:
-	    path (string): path to the documents """
-	doc_list = os.listdir(path)
-	tokens = []
-
-	# print(doc_list[len(doc_list)-1])
-	for i,doc in zip(range(len(doc_list)), doc_list):
-	    f = open(path + doc, 'r')
-	    text = f.read()
-
-	    text = process_raw_text(text)
-
-	    doc_tokens = text.split()
-
-
-	    tokens.append(doc_tokens)
-
-	return tokens
-
-
-
-def process_raw_text(text):
-    """Applies different processing techniques on text
-    
-    Args:
-        text (string): a document as text
-    
-    Returns:
-        string: processed document
-    """
-    punctuation_dict = dict.fromkeys(string.punctuation,' ')
-
-    text = text.lower()		# convert to lowercase
-    text = text.replace('\\n', ' ')
-    text = re.sub('[0-9]', '',text)		# TODO remove digits but keep years
-    text = re.sub(r'(?<!\w)([a-z])\.', r'\1', text)		# remove periods from acronmys, e.g: U.S.A -> USA
-    
-    text = expand_contractions(text)
-    
-    text = text.translate(str.maketrans(punctuation_dict)).strip() # remove punctuations
-    return text
-
-
-
-def expand_contractions(text):
-    """expands common contractions in english language
-    
-    Args:
-        text (string): a document as text
-    
-    Returns:
-        string: document with all contractions expanded
-    """
-    common_contractions = [("'m", " am"), ("n't", " not"), ("'ve", " have"), ("'ll", " will"), ("'s", "s"), ("'d", " would")]
-    for cc in common_contractions:
-        text = re.sub(cc[0], cc[1], text)
-    return text
-
-
-def pickle_item(item, filename):
-
-	outfile = open(filename, 'wb')
-	pickle.dump(item, outfile)
-	outfile.close()
-
-
+    pickler.pickle_item(doc_vectors, 'docvec')
+    pickler.pickle_item(vectorizer, 'vectorizer')
 
 
 if __name__ == "__main__":
